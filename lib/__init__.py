d97bfaba01ce231dd7c86fe9827f0d6979bb012e
Totul <rytotul@gmail.com>
2017-04-19 17:33:47 +0600

    Added pastebin api as library

---
+++ lib/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2017 ISTT_Cyborgs (fb.com/groups/istt.cse.2017)
+Author: Rakibul Yeasin Totul (fb.com/rytotul)
+See the file 'doc/COPYING' for copying permission
+"""
+
+pass
